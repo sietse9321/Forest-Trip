7b0ed2144dc18d455566cab58d128e872c43e349
Koen Polman <KoenPolman_@outlook.com>
2023-09-05 09:38:52 +0200

    feat: new script made

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    float health = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
