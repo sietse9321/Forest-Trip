1050430bae29da7d616de2ef18550bd7cde5859e
Koen Polman <KoenPolman_@outlook.com>
2023-09-05 10:02:01 +0200

    feat: enemy script uitgewerkt

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    float health = 5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] GameObject playerObject;
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float health = 5f;
     // Update is called once per frame
     void Update()
     {
-        
+        //berekent de richting waar de enemy naartoe beweegt 
+        Vector3 moveDirection = (playerObject.transform.position - transform.position).normalized;
+        //gebruikt vervolgens het resultaat van hier boven om richting de speler te bewegen
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
