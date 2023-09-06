5ea9621ca4532781a4d29de4738b4dd79fa7ad58
Koen Polman <KoenPolman_@outlook.com>
2023-09-06 10:06:32 +0200

    feat: prototype enemy created

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    [SerializeField] GameObject playerObject;
-    [SerializeField] float moveSpeed = 1f;
-    [SerializeField] float health = 5f;
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] public float health = 5f;
     // Update is called once per frame
     void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
     {
         //berekent de richting waar de enemy naartoe beweegt 
         Vector3 moveDirection = (playerObject.transform.position - transform.position).normalized;
         //gebruikt vervolgens het resultaat van hier boven om richting de speler te bewegen
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
     }
 }
