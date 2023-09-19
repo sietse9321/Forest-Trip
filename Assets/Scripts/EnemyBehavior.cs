650e648639f76b0f7cd003c16d8d65651461a573
Koen Polman <KoenPolman_@outlook.com>
2023-09-19 10:29:36 +0200

    fix: comment toe gevoegd en veranderd

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,25 +6,25 @@ using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    [SerializeField] GameObject playerObject; //gameobject speler gelinkt zodat de enemy neer hem toe gaat
-    public float health = 3f; //health zodat de enemy kan sterven 
+    [SerializeField] GameObject playerObject; //gameobject player linkes so the enemy can find it
+    public float health = 3f; //health so the enemy can die
     NavMeshAgent agent; //she nav on my mesh till i agent
-    [SerializeField] GameObject hurtSound; //losse game object voor geluid zodat het niet stopt wanneer de enemyGameObject spontaan stopt met bestaan
-    [SerializeField] GameObject deathSound; //lees de comment hierboven
+    [SerializeField] GameObject hurtSound; //seperate game object for sound effects for when the enemy dies the sound does not immediately stop with it
+    [SerializeField] GameObject deathSound; //read comment above
     void Awake()
     {
-        playerObject = GameObject.Find("henk 1 1"); //vind de locatie van de speler
-        agent = gameObject.GetComponent<NavMeshAgent>(); //pakt de nav mesh agent
+        playerObject = GameObject.Find("henk 1 1"); //find the location of the player
+        agent = gameObject.GetComponent<NavMeshAgent>(); //grabs the navmesh agent
     }
 
     void FixedUpdate()
     {
-        agent.SetDestination(playerObject.transform.position);//navigeer naar de speler
-        if (health <= 0)//als de enemy geen health meer heeft
+        agent.SetDestination(playerObject.transform.position);//navigates to the player
+        if (health <= 0)//if the enemy hos no healt
         {
-            GameObject soundToDestroy = Instantiate(deathSound);//sterf geluid
+            GameObject soundToDestroy = Instantiate(deathSound);//dying noise
             Destroy(soundToDestroy,2f);
-            Destroy(gameObject);//sterft daadwerkelijk
+            Destroy(gameObject);//the enemy dies, is ridden from this world
         }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,27 +6,27 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-    [SerializeField] int possibleSpawnPos = 36; //aantal mogelijke lokaties om te instantiaten
-    [SerializeField] int spawnrate = 500; //de interval voor het spawnen van van enemies
-    [SerializeField] float spawnHeight = 5f; // de spawn hoogte van de enemies
-    [SerializeField] float spawnRadius = 25f; //radius spawn cirkel
-    [SerializeField] int count = 0; //teller voor het spawnen van baddies
+    [SerializeField] int possibleSpawnPos = 36; //number of possible spawn location
+    [SerializeField] int spawnRate = 500; //the interval of enemies to spawn
+    [SerializeField] float spawnHeight = 5f; // spawn height of enemies
+    [SerializeField] float spawnRadius = 25f; //radius of the spawn circle
+    [SerializeField] int count = 0; //counter for spawning enemies
 
     private void FixedUpdate()
     {
-        if (count % spawnrate == 0)//als count modulo spawnrate is dan word er een enemy gespawned
+        if (count % spawnRate == 0)//if count is modulo spawn rate then spawn a enemy
         {
             SpawnObjectsInCircle();
         }
         count++;
     }
     /// <summary>
-    /// deze method berekent een cirkel rondom de speler en zet ergens op die radius een enemy
+    /// this method calculates the spawn location of the enemy
     /// </summary>
     void SpawnObjectsInCircle()
     {
-        float angle = Random.Range(1, 37) * Mathf.PI * 2 / possibleSpawnPos;//bepaald een willekeurige van de 36 verschillende richtingen
-        Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle) * spawnRadius, spawnHeight, Mathf.Sin(angle) * spawnRadius);//berekent de plaats om een enemy te spawnen
-        GameObject newObject = Instantiate(enemy, spawnPosition, Quaternion.identity);//spawnt een nieuwe enemy
+        float angle = Random.Range(1, 37) * Mathf.PI * 2 / possibleSpawnPos;//picks one of the 36 random directions to spawn a enemy in
+        Vector3 spawnPosition = transform.position + new Vector3(Mathf.Cos(angle) * spawnRadius, spawnHeight, Mathf.Sin(angle) * spawnRadius);//calculates a place to spawn a enemy in
+        GameObject newObject = Instantiate(enemy, spawnPosition, Quaternion.identity);//spawns a new enemy
     }
 }
