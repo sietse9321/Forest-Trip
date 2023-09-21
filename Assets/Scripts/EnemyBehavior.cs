55d3dadd404a6d748a7ff1e9e8be3935592e2b9e
sietse9321 <99656452+sietse9321@users.noreply.github.com>
2023-09-21 12:26:00 +0200

    Revert "Merge branch 'main' into Feature_Sietse"
    This reverts commit da865e7814c7ea6de353314a01caf0dda88a6201, reversing
    changes made to 17f8bf898a065fd95027c28a12d1c39339a9a425.

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    [SerializeField] GameObject playerObject; 
+    [SerializeField] GameObject playerObject;
     public float health = 3f;
     NavMeshAgent agent;
     [SerializeField] GameObject hurtSound;
@@ -16,39 +16,31 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] GameObject sound3;
     [SerializeField] GameObject sound4;
     [SerializeField] GameObject sound5;
-    [SerializeField] GameObject sound6;
-    [SerializeField] GameObject sound7;
     [SerializeField] GameObject death;
-    
 
+    // Update is called once per frame
     void Awake()
     {
-
-       
         playerObject = GameObject.Find("henk 1 1");
         agent = gameObject.GetComponent<NavMeshAgent>();
-        
-
     }
 
     void FixedUpdate()
     {
         Sound();
-        agent.SetDestination(playerObject.transform.position); //goes towards player trough navmesh
+        agent.SetDestination(playerObject.transform.position);
         if (health <= 0)
         {
-
-            GameObject soundToDestroy = Instantiate(deathSound,transform.position, Quaternion.identity);
-            Destroy(soundToDestroy, 3f);
-
+            GameObject soundToDestroy = Instantiate(deathSound);
+            Destroy(soundToDestroy, 2f);
             GameObject explosion = Instantiate(death, transform.position, Quaternion.identity);
             Destroy(explosion, 3f);
-            Destroy(gameObject);//the enemy gets removed from the scene
+            Destroy(gameObject);
         }
     }
     void Sound()
     {
-        float randomNumber = Random.Range(0, 2000);
+        float randomNumber = Random.Range(0, 1750);
         switch (randomNumber)
         {
             case 1:
@@ -70,14 +62,6 @@ public class EnemyBehavior : MonoBehaviour
             case 5:
                 GameObject soundToDestroy5 = Instantiate(sound5, transform.position, Quaternion.identity);
                 Destroy(soundToDestroy5, 4f);
-                break;
-            case 6:
-                GameObject soundToDestroy6 = Instantiate(sound6, transform.position, Quaternion.identity);
-                Destroy(soundToDestroy6, 4f);
-                break;
-            case 7:
-                GameObject soundToDestroy7 = Instantiate(sound7, transform.position, Quaternion.identity);
-                Destroy(soundToDestroy7, 4f);
                 break;
         }
 
