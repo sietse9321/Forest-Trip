802bab5168dec946342956f65d6caaa83f95fbe5
Koen Polman <KoenPolman_@outlook.com>
2023-09-21 10:44:36 +0200

    fix: added comments

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -14,106 +14,74 @@ public class Walking : MonoBehaviour
     public GameObject running;
     public EnemyBehavior enemy;
     public AudioSource damage;
-    
-
-    // Start is called before the first frame update
     void Start()
     {
-        
-
-        walk.SetActive(false);
+        walk.SetActive(false); //bools are set to false so when the gameplay segment starts the player stands still
         jump.SetActive(false);
         crawl.SetActive(false);
-        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        //check if one of the WASD keys is pressed down to start walking
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("s") || Input.GetKeyDown("d") || Input.GetKeyDown("a"))
         {
             WalkStart();
         }
-        if (Input.GetKeyDown("s"))
-        {
-            WalkStart();
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            WalkStart();
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            WalkStart();
-        }
-        if (Input.GetKeyUp("w"))
+        //checks if one of the WASD keys is let go to stop walking
+        if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("s") || Input.GetKeyUp("d"))
         {
             WalkStop();
         }
-        if (Input.GetKeyUp("a"))
-        {
-            WalkStop();
-        }
-        if (Input.GetKeyUp("s"))
-        {
-            WalkStop();
-        }
-        if (Input.GetKeyUp("d"))
-        {
-            WalkStop();
-        }
+        //checks if the space key is pressed down to start jumping
         if (Input.GetKeyDown("space"))
         {
             JumpStart();
         }
+        //checks if the space key is let go to stop jumping 
         if (Input.GetKeyUp("space"))
         {
             JumpStop();
         }
+        //checks if the left shift key is pressed down to start running
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             RunStart();
         }
+        //checks if the left shift key is let go to stop running
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             RunStop();
         }
+        //checks if the left control key is pressed down to start crouching
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             CrawlStart();
         }
+        //checks if the left control key is let go to stop crouching
         if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             CrawlStop();
         }
+        //checks if the left mouse button is pressed down to fire the pistol
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-           if(gn.magazine > 0)
+            //checks if the gun is empty when the gun is fired
+            if (gn.magazine > 0)
             {
-                shoot.Play();
+                shoot.Play();//plays fire sound
             }
             else
             {
-                jammed.Play();
+                jammed.Play();//plays empty click sound
             }
         }
+        //checks if the r key is pressed down to reload
         if (Input.GetKey(KeyCode.R))
         {
             reload.Play();
         }
-        
-        
-        
-
-
     }
-
-
-
-
-
-
-
+    //methods to to set variables
     void WalkStart()
     {
         walk.SetActive(true);
@@ -146,17 +114,4 @@ public class Walking : MonoBehaviour
     {
         running.SetActive(false);
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
